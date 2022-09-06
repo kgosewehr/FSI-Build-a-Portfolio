5e856a8951ff9685304bfbe6717b0b4bd2ef7dc0
kgosewehr <keithgosewehr@gmail.com>
2022-09-05 22:40:30 -0700

    added css animation

--- app.js
+++ app.js
@@ -1,30 +1,7 @@
-const ul_1 = document.querySelector(".option1");
-const ul_2 = document.querySelector(".option2");
-const ul_3 = document.querySelector(".option3");
-
-const q1 = document.querySelector("q1");
-const q2 = document.querySelector("q2");
-const q3 = document.querySelector("q3");
-
-const survey = document.querySelector(".survey");
-const end = document.querySelector(".end");
-
-
-ul_1.addEventListener("click", function(){
-    console.log("it worked");  
-  q1.style.display = "none";
-  q2.style.display = "block";
-});
-
-
-ul_2.addEventListener("click", function(){
-    q2.style.display = "none";
-    q3.style.display = "block";
-});
-
-ul_3.addEventListener("click", function(){
-    q3.style.display = "none";
-    survey.style.display = "none";
-    end.style.display = "block";
-
-});
+const text = document.querySelector(".text");
+text.innerHTML = text.innerText
+	.split("")
+	.map(
+		(char, i) => `<span style="transform:rotate(${i * 9.6}deg)">${char}</span>`
+	)
+	.join("");
