ceb58e56e5925a7b38381aebcad2d94de988fc05
kgosewehr <keithgosewehr@gmail.com>
2022-09-05 17:38:14 -0700

    css changes

--- app.js
+++ app.js
@@ -6,22 +6,25 @@ const q1 = document.querySelector("q1");
 const q2 = document.querySelector("q2");
 const q3 = document.querySelector("q3");
 
-const survey = document.querySelector("survey");
-const end = document.querySelector("end");
+const survey = document.querySelector(".survey");
+const end = document.querySelector(".end");
+
 
 ul_1.addEventListener("click", function(){
-    q1.style.display ="none"
-    q2.style.display ="block"
-})
+    console.log("it worked");  
+  q1.style.display = "none";
+  q2.style.display = "block";
+});
+
 
 ul_2.addEventListener("click", function(){
-    q2.style.display ="none"
-    q3.style.display ="block"
-})
+    q2.style.display = "none";
+    q3.style.display = "block";
+});
 
 ul_3.addEventListener("click", function(){
-    q3.style.display = "none"
-    survey.style.display = "none"
-    end.style.display = "block"
+    q3.style.display = "none";
+    survey.style.display = "none";
+    end.style.display = "block";
 
-})
+});
