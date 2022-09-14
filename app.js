9176f2bd6964703927489f8dbf6663ccf600df6d
kgosewehr <keithgosewehr@gmail.com>
2022-09-13 20:38:16 -0700

    Changes

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const text = document.querySelector(".text");
 text.innerHTML = text.innerText
 	.split("")
 	.map(
-		(char, i) => `<span style="transform:rotate(${i * 9.7}deg)">${char}</span>`
+		(char, i) => `<span style = "transform : rotate(${i * 9.5}deg)">${char}</span>`
 	)
 	.join("");
